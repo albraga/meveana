de8a19b22cc81d1ad8af6f8abc13c0b3ff8d7147
Alexandre Braga <alexandre@brahgah.com>
2015-10-28 13:37:58 +0000

    /web/

--- views/site/index.php
+++ views/site/index.php
@@ -9,11 +9,11 @@ $this->title = 'Meveana Pizzaria';
     <div class="body-content">
 
         <div class="row">
-             <a class="btn btn-lg btn-block btn-danger" href="../pedido/create" role="button">Novo Pedido</a>
-             <a class="btn btn-lg btn-block btn-warning" href="../cliente/create" role="button">Novo Cliente</a>
-             <a class="btn btn-lg btn-block btn-info" href="../produto" role="button">Produtos</a> 
-             <a class="btn btn-lg btn-block btn-primary" href="../entregador" role="button">Entregadores</a>
-             <a class="btn btn-lg btn-block btn-primary" href="../terceirizada" role="button">Terceirizadas</a> 
+             <a class="btn btn-lg btn-block btn-danger" href="/web/pedido/create" role="button">Novo Pedido</a>
+             <a class="btn btn-lg btn-block btn-warning" href="/web/cliente/create" role="button">Novo Cliente</a>
+             <a class="btn btn-lg btn-block btn-info" href="/web/produto" role="button">Produtos</a> 
+             <a class="btn btn-lg btn-block btn-primary" href="/web/entregador" role="button">Entregadores</a>
+             <a class="btn btn-lg btn-block btn-primary" href="/web/terceirizada" role="button">Terceirizadas</a> 
         </div>
 
     </div>
